1d24723ee28c19331d9d286b2a9963a92bc0f668
toplus <toplus90@alice.it>
2013-10-22 20:21:03 +0200

    modified graphics

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,7 +1,11 @@
 
 function traduci(nome){
 	//nome_scherzo = getCookie("nome_scherzo");
+	nomi = [ 'Rombo di Tuono', 'Toro seduto', 'Nuvola rossa', 'Occhio di volpe', 'Gatto pazzo', 'Stella coperta', 'Buona strada',
+			'Spirito', 'Arcobaleno', 'Condor del Sole', 'Aquila del Sole', 'Condor selvaggio', 'Spirito guerriero', 'Orso magro',
+			'Antilope', 'Spirito del Cervo', 'Senza paura', 'Capo alto', "Tromba d'aria", 'Piccolo lupo' ];
 	nome_scherzo = window.localStorage.getItem("nome");
+	nome = nome.toLowerCase();
 	if(!nome)
 		return;
 	if(nome == nome_scherzo)
@@ -10,33 +14,8 @@ function traduci(nome){
 		value = 0;
 		for( i=0; i < nome.length; i++)
 			value += nome.charCodeAt(i);
-		opz = (value%12)+1;
-		switch(opz){
-			case 1: nome2 = 'Rombo di Tuono';
-				break;
-			case 2: nome2 = 'Toro seduto';
-				break;
-			case 3: nome2 = 'Nuvola rossa';
-				break;
-			case 4: nome2 = 'Occhio di volpe';
-				break;
-			case 5: nome2 = 'Gatto pazzo';
-				break;
-			case 6: nome2 = 'Stella coperta';
-				break;
-			case 7: nome2 = 'Spirito';
-				break;
-			case 8: nome2 = 'Arcobaleno';
-				break;	
-			case 9: nome2 = 'Condor del Sole';
-				break;
-			case 10: nome2 = 'Aquila del Sole';
-				break;
-			case 11: nome2 = 'Condor selvaggio';
-				break;
-			case 12: nome2 = 'Spirito guerriero';
-				break;	
-		}
+		opz = (value%nomi.length)+1;
+		nome2 = nomi[opz];
 		document.getElementById('nome2').innerHTML = nome2;	
 	}
 }
